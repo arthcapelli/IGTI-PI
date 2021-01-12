a387a60b153e717b5367d655b137dec49e4c35ae
Arthur Capelli <71565674+arthcapelli@users.noreply.github.com>
2021-01-12 20:15:18 -0300

    Add files via upload

---
+++ Módulo III/ListaCompras/src/listacompras/Main.java
@@ -0,0 +1,11 @@
+package listacompras;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ListaCompras listaCompras = new ListaCompras();
+		listaCompras.iniciar();
+
+	}
+
+}
---
+++ Módulo III/SequenciaParentesesColchetes/src/sequencia/Sequencia.java
@@ -0,0 +1,79 @@
+package sequencia;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Sequencia {
+	private Scanner scanner = new Scanner(System.in);
+
+	private String mostrarMenu() {
+		System.out.println("Digite a sequencia para verificação ou digite sair para encerrar:");
+
+		return scanner.nextLine();
+	}
+
+	public void iniciar() {
+		// Sequencias válidas () - [] - (()) - [[]] - [()] - ([])
+		// Sequencias inválidas (( - )) - [[ - ]] - ([))
+
+		String sequencia = mostrarMenu();
+		while (!sequencia.equals("sair")) {
+			Stack<String> stack = new Stack<>();
+			boolean isSequenciaOk = true;
+
+			for (int i = 0; i < sequencia.length(); i++) {
+				if (!isSequenciaOk) {
+					break;
+				}
+				char c = sequencia.charAt(i);
+
+				switch (c) {
+				case ')':
+					if (stack.isEmpty()) {
+						System.out.println("Sequencia inválida!");
+						sequencia = mostrarMenu();
+						isSequenciaOk = false;
+
+					} else {
+						String elemento = stack.pop();
+						if (!elemento.equals("(")) {
+							System.out.println("Sequencia inválida!");
+							sequencia = mostrarMenu();
+							isSequenciaOk = false;
+						}
+					}
+					break;
+
+				case ']':
+					if (stack.isEmpty()) {
+						System.out.println("Sequencia inválida!");
+						sequencia = mostrarMenu();
+						isSequenciaOk = false;
+
+					} else {
+						String elemento = stack.pop();
+						if (!elemento.equals("[")) {
+							System.out.println("Sequencia inválida!");
+							sequencia = mostrarMenu();
+							isSequenciaOk = false;
+						}
+					}
+					break;
+
+				default:
+					stack.push(String.valueOf(c));
+
+				}
+
+			}
+			if (!sequencia.equals("sair")) {
+				if (stack.isEmpty()) {
+					System.out.println("Sequencia correta!");
+				} else {
+					System.out.println("Sequencia inválida!");
+				}
+				sequencia = mostrarMenu();
+			}
+		}
+	}
+}
